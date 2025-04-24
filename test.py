0b706971c9ba6706cb18478eb870130ae07e9bcf
NAMBU KEERTHI R <nambu935@gmail.com>
2025-04-25 00:19:19 +0530

    Update test.py

--- test.py
+++ test.py
@@ -76,113 +76,113 @@ def json_function(data):
 
 # DATA EXTRACTION 
 def data_extraction():
-json_file1 = "https://zomato-aws-project-guvi.s3.eu-north-1.amazonaws.com/file1.json"
-data1 = aws_bucket_connection(json_file1)       
-returned_data1 =  json_function(data1) 
-df1 = pd.DataFrame([row.split(", ") for row in returned_data1], columns=['Restaurant_Name', 'Restaurant_ID', 'Price_range','Cuisines','Average_cost','Rating_text',
-                                                               'Rating_color','Rating_votes','Rating','Address','City','City_id','Country_id','Locality'])
-
-#-------
-
-json_file2 = "https://zomato-aws-project-guvi.s3.eu-north-1.amazonaws.com/file2.json"
-data2 = aws_bucket_connection(json_file2) 
-returned_data2 =  json_function(data2)
-
-max_columns = 14  # or whatever your intended number of columns is
-dataF2_fixed = []
-
-for row in returned_data2:
-    row_values = row.split(',')
-    if len(row_values) > max_columns:
-        row_values = row_values[:max_columns]  # Truncate to the desired number of columns
-    elif len(row_values) < max_columns:
-        row_values.extend([''] * (max_columns - len(row_values)))  # Add empty values for missing columns
-    dataF2_fixed.append(row_values)
-
-# Now create the DataFrame with consistent data
-df2 = pd.DataFrame(dataF2_fixed, columns=['Restaurant_Name', 'Restaurant_ID', 'Price_range', 'Cuisines', 'Average_cost',
-                                           'Rating_text', 'Rating_color', 'Rating_votes', 'Rating', 'Address', 'City',
-                                           'City_id', 'Country_id', 'Locality'])
-#-------
-
-json_file3 = "https://zomato-aws-project-guvi.s3.eu-north-1.amazonaws.com/file3.json"
-data3 = aws_bucket_connection(json_file3) 
-returned_data3=  json_function(data3)
-
-max_columns = 14  # or whatever your intended number of columns is
-dataF3_fixed = []
-
-for row in returned_data3:
-    row_values = row.split(',')
-    if len(row_values) > max_columns:
-        row_values = row_values[:max_columns]  # Truncate to the desired number of columns
-    elif len(row_values) < max_columns:
-        row_values.extend([''] * (max_columns - len(row_values)))  # Add empty values for missing columns
-    dataF3_fixed.append(row_values)
-
-# Now create the DataFrame with consistent data
-df3 = pd.DataFrame(dataF3_fixed, columns=['Restaurant_Name', 'Restaurant_ID', 'Price_range', 'Cuisines', 'Average_cost',
-                                           'Rating_text', 'Rating_color', 'Rating_votes', 'Rating', 'Address', 'City',
-                                           'City_id', 'Country_id', 'Locality'])
-
-#-------
-
-json_file4 = "https://zomato-aws-project-guvi.s3.eu-north-1.amazonaws.com/file4.json"
-data4 = aws_bucket_connection(json_file4)
-returned_data4 =  json_function(data4)
-
-max_columns = 14  # or whatever your intended number of columns is
-dataF4_fixed = []
-
-for row in returned_data4:
-    row_values = row.split(',')
-    if len(row_values) > max_columns:
-        row_values = row_values[:max_columns]  # Truncate to the desired number of columns
-    elif len(row_values) < max_columns:
-        row_values.extend([''] * (max_columns - len(row_values)))  # Add empty values for missing columns
-    dataF4_fixed.append(row_values)
-
-# Now create the DataFrame with consistent data
-df4 = pd.DataFrame(dataF4_fixed, columns=['Restaurant_Name', 'Restaurant_ID', 'Price_range', 'Cuisines', 'Average_cost',
-                                           'Rating_text', 'Rating_color', 'Rating_votes', 'Rating', 'Address', 'City',
-                                           'City_id', 'Country_id', 'Locality'])
-
-#-------
-
-json_file5 = "https://zomato-aws-project-guvi.s3.eu-north-1.amazonaws.com/file5.json"
-data5 = aws_bucket_connection(json_file5)
-returned_data5 =  json_function(data5)
-
-max_columns = 14  # or whatever your intended number of columns is
-dataF5_fixed = []
-
-for row in returned_data5:
-    row_values = row.split(',')
-    if len(row_values) > max_columns:
-        row_values = row_values[:max_columns]  # Truncate to the desired number of columns
-    elif len(row_values) < max_columns:
-        row_values.extend([''] * (max_columns - len(row_values)))  # Add empty values for missing columns
-    dataF5_fixed.append(row_values)
-
-# Now create the DataFrame with consistent data
-df5 = pd.DataFrame(dataF5_fixed, columns=['Restaurant_Name', 'Restaurant_ID', 'Price_range', 'Cuisines', 'Average_cost',
-                                           'Rating_text', 'Rating_color', 'Rating_votes', 'Rating', 'Address', 'City',
-                                           'City_id', 'Country_id', 'Locality'])
-
-df_concat = pd.concat([df1, df2,df3,df4,df5], ignore_index=True)
-df_original = df_concat.drop_duplicates(ignore_index=True)
-
-df_original['Cuisines'] = df_original['Cuisines'].str.replace('Continental', '', regex=True)
-df_original['Rating_text'] = df_original['Rating_text'].str.replace(' Very Good', 'Very Good', regex=True)
-df_original['Rating_text'] = df_original['Rating_text'].str.replace(' Excellent', 'Excellent', regex=True)
-df_original['Rating_text'] = df_original['Rating_text'].str.replace(' Poor', 'Poor', regex=True)
-df_original['Rating_text'] = df_original['Rating_text'].str.replace(' Average', 'Average', regex=True)
-df_original['Rating_text'] = df_original['Rating_text'].str.replace(' Not rated', 'Not rated', regex=True)
-df_original['Rating_text'] = df_original['Rating_text'].str.replace(' Good', 'Good', regex=True)
-df_original = df_original[~df_original['Rating_text'].isin([' North Indian  Chinese  Continental  Italian', ' Continental  American  Italian  Bakery',' Chinese  North Indian',' 350',' 200',' 300',' 250'])]
-df_original = df_original[~df_original['City_id'].isin([' New Delhi', ' Level 3  City Square Mall  Rajouri Garden  New Delhi',' D-15  Bhai Ji Market  Near Bishanpura Village  Sector 58  Noida',' Faridabad',
-                                                        ' 31  2nd Floor  Hauz Khas Village  New Delhi',' Davenport'])]
-df_original.reset_index(inplace=True)
+        json_file1 = "https://zomato-aws-project-guvi.s3.eu-north-1.amazonaws.com/file1.json"
+        data1 = aws_bucket_connection(json_file1)       
+        returned_data1 =  json_function(data1) 
+        df1 = pd.DataFrame([row.split(", ") for row in returned_data1], columns=['Restaurant_Name', 'Restaurant_ID', 'Price_range','Cuisines','Average_cost','Rating_text',
+                                                                       'Rating_color','Rating_votes','Rating','Address','City','City_id','Country_id','Locality'])
+        
+        #-------
+        
+        json_file2 = "https://zomato-aws-project-guvi.s3.eu-north-1.amazonaws.com/file2.json"
+        data2 = aws_bucket_connection(json_file2) 
+        returned_data2 =  json_function(data2)
+        
+        max_columns = 14  # or whatever your intended number of columns is
+        dataF2_fixed = []
+        
+        for row in returned_data2:
+            row_values = row.split(',')
+            if len(row_values) > max_columns:
+                row_values = row_values[:max_columns]  # Truncate to the desired number of columns
+            elif len(row_values) < max_columns:
+                row_values.extend([''] * (max_columns - len(row_values)))  # Add empty values for missing columns
+            dataF2_fixed.append(row_values)
+        
+        # Now create the DataFrame with consistent data
+        df2 = pd.DataFrame(dataF2_fixed, columns=['Restaurant_Name', 'Restaurant_ID', 'Price_range', 'Cuisines', 'Average_cost',
+                                                   'Rating_text', 'Rating_color', 'Rating_votes', 'Rating', 'Address', 'City',
+                                                   'City_id', 'Country_id', 'Locality'])
+        #-------
+        
+        json_file3 = "https://zomato-aws-project-guvi.s3.eu-north-1.amazonaws.com/file3.json"
+        data3 = aws_bucket_connection(json_file3) 
+        returned_data3=  json_function(data3)
+        
+        max_columns = 14  # or whatever your intended number of columns is
+        dataF3_fixed = []
+        
+        for row in returned_data3:
+            row_values = row.split(',')
+            if len(row_values) > max_columns:
+                row_values = row_values[:max_columns]  # Truncate to the desired number of columns
+            elif len(row_values) < max_columns:
+                row_values.extend([''] * (max_columns - len(row_values)))  # Add empty values for missing columns
+            dataF3_fixed.append(row_values)
+        
+        # Now create the DataFrame with consistent data
+        df3 = pd.DataFrame(dataF3_fixed, columns=['Restaurant_Name', 'Restaurant_ID', 'Price_range', 'Cuisines', 'Average_cost',
+                                                   'Rating_text', 'Rating_color', 'Rating_votes', 'Rating', 'Address', 'City',
+                                                   'City_id', 'Country_id', 'Locality'])
+        
+        #-------
+        
+        json_file4 = "https://zomato-aws-project-guvi.s3.eu-north-1.amazonaws.com/file4.json"
+        data4 = aws_bucket_connection(json_file4)
+        returned_data4 =  json_function(data4)
+        
+        max_columns = 14  # or whatever your intended number of columns is
+        dataF4_fixed = []
+        
+        for row in returned_data4:
+            row_values = row.split(',')
+            if len(row_values) > max_columns:
+                row_values = row_values[:max_columns]  # Truncate to the desired number of columns
+            elif len(row_values) < max_columns:
+                row_values.extend([''] * (max_columns - len(row_values)))  # Add empty values for missing columns
+            dataF4_fixed.append(row_values)
+        
+        # Now create the DataFrame with consistent data
+        df4 = pd.DataFrame(dataF4_fixed, columns=['Restaurant_Name', 'Restaurant_ID', 'Price_range', 'Cuisines', 'Average_cost',
+                                                   'Rating_text', 'Rating_color', 'Rating_votes', 'Rating', 'Address', 'City',
+                                                   'City_id', 'Country_id', 'Locality'])
+        
+        #-------
+        
+        json_file5 = "https://zomato-aws-project-guvi.s3.eu-north-1.amazonaws.com/file5.json"
+        data5 = aws_bucket_connection(json_file5)
+        returned_data5 =  json_function(data5)
+        
+        max_columns = 14  # or whatever your intended number of columns is
+        dataF5_fixed = []
+        
+        for row in returned_data5:
+            row_values = row.split(',')
+            if len(row_values) > max_columns:
+                row_values = row_values[:max_columns]  # Truncate to the desired number of columns
+            elif len(row_values) < max_columns:
+                row_values.extend([''] * (max_columns - len(row_values)))  # Add empty values for missing columns
+            dataF5_fixed.append(row_values)
+        
+        # Now create the DataFrame with consistent data
+        df5 = pd.DataFrame(dataF5_fixed, columns=['Restaurant_Name', 'Restaurant_ID', 'Price_range', 'Cuisines', 'Average_cost',
+                                                   'Rating_text', 'Rating_color', 'Rating_votes', 'Rating', 'Address', 'City',
+                                                   'City_id', 'Country_id', 'Locality'])
+        
+        df_concat = pd.concat([df1, df2,df3,df4,df5], ignore_index=True)
+        df_original = df_concat.drop_duplicates(ignore_index=True)
+        
+        df_original['Cuisines'] = df_original['Cuisines'].str.replace('Continental', '', regex=True)
+        df_original['Rating_text'] = df_original['Rating_text'].str.replace(' Very Good', 'Very Good', regex=True)
+        df_original['Rating_text'] = df_original['Rating_text'].str.replace(' Excellent', 'Excellent', regex=True)
+        df_original['Rating_text'] = df_original['Rating_text'].str.replace(' Poor', 'Poor', regex=True)
+        df_original['Rating_text'] = df_original['Rating_text'].str.replace(' Average', 'Average', regex=True)
+        df_original['Rating_text'] = df_original['Rating_text'].str.replace(' Not rated', 'Not rated', regex=True)
+        df_original['Rating_text'] = df_original['Rating_text'].str.replace(' Good', 'Good', regex=True)
+        df_original = df_original[~df_original['Rating_text'].isin([' North Indian  Chinese  Continental  Italian', ' Continental  American  Italian  Bakery',' Chinese  North Indian',' 350',' 200',' 300',' 250'])]
+        df_original = df_original[~df_original['City_id'].isin([' New Delhi', ' Level 3  City Square Mall  Rajouri Garden  New Delhi',' D-15  Bhai Ji Market  Near Bishanpura Village  Sector 58  Noida',' Faridabad',
+                                                                ' 31  2nd Floor  Hauz Khas Village  New Delhi',' Davenport'])]
+        df_original.reset_index(inplace=True)
 
 
 #-------------------------------------------------------------------
