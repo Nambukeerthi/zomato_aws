d7af51760ea0034a6672ab4691ad440ae5ee5f91
NAMBU KEERTHI R <nambu935@gmail.com>
2025-04-24 23:26:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -231,9 +231,9 @@ if st.button("CLICK HERE"):
   url2 = "https://zomato-aws-project-guvi.s3.eu-north-1.amazonaws.com/print.py"
   # Fetch the file content
   response2 = requests.get(url2)
-  code = response2.text
+  response2.text
   # Execute the code inside the module's namespace
-  exec(code)
+  #exec(code)
   
 
 
